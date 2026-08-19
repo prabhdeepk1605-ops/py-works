e86229b2e23f69a96c19202dd4ae9a9806b9bd84
prabhdeepk1605-ops <prabhdeepk1605@gmail.com>
2026-08-19 16:15:18 +0530

    base start

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST,OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return responsekv
+    return response
 
 
 
